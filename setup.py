26c37ab9f855a9bf9e4a1f9707ba20f2c27a0701
Antoine Cezar <antoine@cezar.fr>
2018-03-11 22:07:18 +0100

    Release v1.2.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='timeoutcontext',
-    version='1.1.1',
+    version='1.2.0',
     description="A signal based timeout context manager",
     long_description=readme + '\n\n' + history,
     author="Antoine Cezar",
